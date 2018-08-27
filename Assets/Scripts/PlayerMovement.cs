2b019d864bd1cc561272f6f22910be5e00f11a32
Cody Ware <39965219+codyw124@users.noreply.github.com>
2018-08-27 08:18:15 -0400

    stuff i have been working on for a couple weeks

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller_;
+
+    float horizontalMove_ = 0f;
+
+    float runSpeed_ = 40f;
+
+    bool jump_ = true;
+
+	// get user input
+	void Update ()
+    {
+        horizontalMove_ = Input.GetAxisRaw("Horizontal") * runSpeed_;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Debug.Log(jump_);
+            jump_ = true;
+            Debug.Log(jump_);
+        }
+	}
+
+    //apply user input
+    private void FixedUpdate()
+    {
+
+        controller_.Move(horizontalMove_ * Time.fixedDeltaTime, false, jump_);
+
+        //jump_ = false;
+    }
+}
